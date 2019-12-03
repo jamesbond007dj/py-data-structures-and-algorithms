eb0a960fda73fec92c410355e7ec627bda306208
jamesbond007dj <ilkerzaimoglu@gmail.com>
2019-12-02 23:15:28 -0800

    challenge done in 3 different method

---
+++ challenges/array_reverse/array_reverse.py
@@ -0,0 +1,27 @@
+# def Reverse(arr):
+#     return [ele for ele in reversed(arr)]
+
+
+# arr = [1, 5, 8, 10]
+# print(Reverse(arr))
+
+# Above Method 1 : Using the reversed() built in function. I learnt it from geeksforgeeks.org
+
+
+# def Reverse(arr):
+#     arr.reverse()
+#     return arr
+
+# arr = [1, 5, 8, 10]
+# print(Reverse(arr))
+
+# Above Method 2 : Using the reverse() built in function. I learnt it from geeksforgeeks.org
+
+def Reverse(arr):
+    new_arr = arr[::-1]
+    return new_arr
+
+arr = [1, 5, 8, 10]
+print(Reverse(arr))
+
+# Above Method 3 : rversing an array using slicing tech. I learnt it from geeksforgeeks.org
