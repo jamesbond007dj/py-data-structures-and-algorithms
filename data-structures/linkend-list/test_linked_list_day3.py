553a718c66e288f3776e69402c91433097a9e760
jamesbond007dj <ilkerzaimoglu@gmail.com>
2019-12-11 05:28:43 -0800

    kth from end tests for size one added

--- data-structures/linkend-list/test_linked_list_day3.py
+++ data-structures/linkend-list/test_linked_list_day3.py
@@ -8,21 +8,35 @@ for el in values:
     linked_list.insert(el)
 
 
-def test_ll_kth_from_end_one():
+def test_ll_kth_from_end_middle():
     assert 4 == linked_list.ll_kth_from_end(1)
 
-def test_ll_kth_from_end_two():
+def test_ll_kth_from_end_end():
     assert 2 == linked_list.ll_kth_from_end(0)
 
-def test_ll_kth_from_end_three():
+def test_ll_kth_from_end_head():
     assert 6 == linked_list.ll_kth_from_end(2)
 
-def test_ll_kth_from_end_four():
+def test_ll_kth_from_end_same_length():
     assert 'Exception' == linked_list.ll_kth_from_end(3)
 
 def test_ll_kth_from_end_negative():
     assert 'Negative k is not valid' == linked_list.ll_kth_from_end(-1)
-    
+
+def test_ill_kth_from_end_size_one():
+    linked_list_one = LinkedList()
+    values = [6]
+    for el in values:
+        linked_list_one.insert(el)
+    assert 'Exception' == linked_list_one.ll_kth_from_end(1)
+
+def test_ill_kth_from_end_size_one2():
+    linked_list_one = LinkedList()
+    values = [6]
+    for el in values:
+        linked_list_one.insert(el)
+    assert 6 == linked_list_one.ll_kth_from_end(0)
+
 @pytest.mark.skip('pending')
 def test_ill_kth_from_end_empty():
     linked_list_one = LinkedList()
