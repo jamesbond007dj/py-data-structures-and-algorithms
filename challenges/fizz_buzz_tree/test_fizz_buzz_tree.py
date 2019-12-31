c49c8e238041585b37b4aaa915f040da96f1880f
jamesbond007dj <ilkerzaimoglu@gmail.com>
2019-12-31 01:45:32 -0800

    test corrected

--- challenges/fizz_buzz_tree/test_fizz_buzz_tree.py
+++ challenges/fizz_buzz_tree/test_fizz_buzz_tree.py
@@ -1,6 +1,10 @@
 import pytest
 from fizz_buzz_tree import Node , BinaryTree , BinarySearchTree , fizz_buzz_tree
 
+"""
+BASIC TEST
+"""
+
 def test_tree_instance():
     tree = BinaryTree()
     assert tree.root is None
@@ -26,6 +30,11 @@ def value_tree():
     tree.add(12)
     return tree
 
+    """
+    HAPPY FEET TEST
+
+    """
+
 def test_fizz_buzz_tree(value_tree):
     fizz_buzz_tree(value_tree)
     assert value_tree.root.value == 9
@@ -33,14 +42,5 @@ def test_fizz_buzz_tree(value_tree):
     assert value_tree.root.right.left.value == 12
     assert value_tree.root.right.right.right.value == 45
 
-def test_fizz_buzz_tree_2(value_tree):
-    fizz_buzz_tree(value_tree)
-    assert value_tree.root.value == 9
-    assert value_tree.root.left.left.value == 2
-    assert value_tree.root.right.left.value == 12
-    assert value_tree.root.right.right.right.value == 45
 
-def test_one_value(value_tree):
-    value_tree.root = Node(9)
-    expected = fizz_buzz_tree(9)
-    assert value_tree.root.value == expected
+
